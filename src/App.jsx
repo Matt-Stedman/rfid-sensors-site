5e761e4c91663666740266a46af91811ab724022
Matt Stedman <matts951@gmail.com>
2024-06-17 10:05:10 +0100

    nicer colour chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,62 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-// import ColourChart from "./components/ColourChart";
-// import { mapNumberToColour } from "./science/number_util";
+import ColourChart from "./components/ColourChart";
+import Timer from "./components/Timer";
 
 const col_chart = {
 	name: "temperature",
 	safe: [0, 50],
-	range_vals: [-20, -10, 0, 5, 10, 15, 20, 30, 40],
+	range_vals: [-20, -10, 0, 10, 20, 30, 40, 50, 60],
 	range_cols: [
-		[0, 0, 255], // -20°C: Very blue
-		[0, 127, 255], // -10°C: Lighter blue
-		[0, 255, 255], // 0°C: Cyan
-		[0, 255, 127], // 5°C: Light greenish-blue
-		[127, 255, 255], // 10°C: Light cyan
-		[128, 128, 128], // 15°C: Grey
-		[255, 255, 0], // 20°C: Yellow
-		[255, 127, 0], // 30°C: Orange
-		[255, 0, 0], // 40°C: Red hot
+		[135, 206, 250], // -20°C: Sky blue
+		[173, 216, 230], // -10°C: Light sky blue
+		[176, 224, 230], // 0°C: Powder blue
+		[211, 211, 211], // 10°C: Light grey
+		[240, 240, 240], // 20°C: Very light grey
+		[240, 128, 128], // 30°C: Light coral (more vibrant red)
+		[255, 99, 71], // 40°C: Tomato (vibrant red)
+		[255, 69, 0], // 50°C: Orange red
+		[255, 0, 0], // 60°C: Red
 	],
 	unit: "°C",
 };
 
 function App() {
 	// Tests
-	// const [temperature, setTemperature] = useState(20);
+	const [temperature, setTemperature] = useState(0);
 	console.log(col_chart);
 
-	const [scannedData, setScannedData] = useState("");
-	const [errorMessage, setErrorMessage] = useState("");
+	const [NDEFReader, setNDEFReader] = useState(null);
+	const [errorMessage, setErrorMessage] = useState(null);
+	const [noteData, setNoteData] = useState(null);
+	const [extraElement, setExtraElement] = useState();
+
+	const onReading = (event) => {
+		const decoder = new TextDecoder();
+		setNoteData(null);
+		setExtraElement(null);
+		for (const record of event.message.records) {
+			const text = decoder.decode(record.data);
+			console.log("Scanned Data:", text);
+			const temperature_str = "temperature=";
+			if (text.startsWith(temperature_str)) {
+				const value_to_set = parseFloat(text.replace(temperature_str, ""));
+				setTemperature(value_to_set);
+				setNoteData(value_to_set);
+			} else {
+				setNoteData(text);
+				setExtraElement(
+					<Timer
+						totalTime={5}
+						title="Tap again!"
+						onComplete={() => {
+							setExtraElement(null);
+						}}
+					/>
+				);
+			}
+		}
+	};
 
 	useEffect(() => {
 		const checkNfcSupport = async () => {
@@ -35,16 +64,7 @@ function App() {
 				try {
 					const ndef = new window.NDEFReader();
 					await ndef.scan();
-					ndef.onreading = (event) => {
-						const decoder = new TextDecoder();
-						for (const record of event.message.records) {
-							const text = decoder.decode(record.data);
-							console.log("Scanned Data:", text);
-							setScannedData(text);
-							// Process the scanned data as needed, for example, update the URL with query params
-							window.location.search = `?param=${text}`;
-						}
-					};
+					ndef.onreading = onReading;
 				} catch (error) {
 					console.error("Error reading NFC:", error);
 					setErrorMessage("NFC reading error: " + error.message);
@@ -55,31 +75,49 @@ function App() {
 		};
 
 		checkNfcSupport();
-	}, []);
+	}, [NDEFReader]);
 
 	return (
-		<div>
-			<h1>Scan your NFC tag</h1>
-			{errorMessage && <p>{errorMessage}</p>}
-			{scannedData && <p>Scanned Data: {scannedData}</p>}
-		</div>
-		// <>
-		// 	<header>
-		// 		<>RFID Temperature Sensor</>
-		// 	</header>
-		// 	<div className="App">
-		// 		<ColourChart
-		// 			test={col_chart}
-		// 			mappedNumber={temperature}
-		// 			mappedColour={mapNumberToColour(temperature, col_chart.range_cols, col_chart.range_vals)}
-		// 		/>
-		// 	</div>
-		// 	<footer>
-		// 		<span>
-		// 			made by <strong>Hard Stuff</strong>
-		// 		</span>
-		// 	</footer>
-		// </>
+		<>
+			<header>
+				<>RFID Temperature Sensor</>
+			</header>
+			<div className="App">
+				<h1>Scan the temperature tag</h1>
+				{!NDEFReader && errorMessage && (
+					<button
+						onClick={() => {
+							try {
+								const ndef = new window.NDEFReader();
+								setNDEFReader(ndef);
+							} catch (error) {
+								console.log("Argh! " + error);
+							}
+						}}
+					>
+						Click for NFC
+					</button>
+				)}
+				{extraElement}
+				{errorMessage ? (
+					<p>{errorMessage}</p>
+				) : (
+					<>
+						{noteData && (
+							<p style={{ backgroundColor: "yellow", border: "2px solid #999933", borderRadius: "5px" }}>
+								Scanned Data: {noteData}
+							</p>
+						)}
+						<ColourChart test={col_chart} mappedNumber={temperature} />
+					</>
+				)}
+			</div>
+			<footer>
+				<span>
+					made by <strong>Matt Stedman</strong>
+				</span>
+			</footer>
+		</>
 	);
 }
 
--- src/components/ColourChart.jsx
+++ src/components/ColourChart.jsx
@@ -1,8 +1,32 @@
 import React, { useState, useEffect } from "react";
 
-const ColourChart = ({ test, mappedNumber, mappedColour }) => {
+export const mapNumberToColour = (inputNumber, colourRange, numberRange) => {
+	const interpolateColour = (colour1, colour2, weight) => {
+		let a = [
+			Math.round(colour1[0] + (colour2[0] - colour1[0]) * weight),
+			Math.round(colour1[1] + (colour2[1] - colour1[1]) * weight),
+			Math.round(colour1[2] + (colour2[2] - colour1[2]) * weight),
+		];
+		return a;
+	};
+	for (let i = 0; i < numberRange.length - 1; i++) {
+		const num1 = numberRange[i];
+		const num2 = numberRange[i + 1];
+		const colour1 = colourRange[i];
+		const colour2 = colourRange[i + 1];
+
+		if (inputNumber >= num1 && inputNumber <= num2) {
+			const weight = (inputNumber - num1) / (num2 - num1);
+			return interpolateColour(colour1, colour2, weight);
+		}
+	}
+	return null; // Return null if the input number is outside the defined number range
+};
+
+const ColourChart = ({ test, mappedNumber = 0 }) => {
 	const [colourRange, numberRange, unit] = [test?.range_cols, test?.range_vals, test?.unit || ""];
 	const [arrowPosition, setArrowPosition] = useState(0);
+	const [mappedColour, setMappedColouer] = useState([0, 0, 0]);
 
 	useEffect(() => {
 		const rangeCount = numberRange.length;
@@ -30,7 +54,11 @@ const ColourChart = ({ test, mappedNumber, mappedColour }) => {
 			setArrowPosition(mappedNumber >= numberRange[numberRange.length - 1] ? 100 : 0);
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [test, mappedNumber, mappedColour, numberRange]);
+	}, [test, mappedNumber, numberRange]);
+
+	useEffect(() => {
+		setMappedColouer(mapNumberToColour(mappedNumber - 5, colourRange, numberRange));
+	}, [mappedColour]);
 
 	return (
 		<div
@@ -49,19 +77,19 @@ const ColourChart = ({ test, mappedNumber, mappedColour }) => {
 				<div
 					style={{
 						position: "absolute",
-						top: arrowPosition ? `calc(${arrowPosition}% + 10px)` : "-10px",
+						top: arrowPosition ? `calc(${arrowPosition}% - 20px)` : "-10px",
 						right: "0px",
 						display: "flex",
 						alignItems: "center",
 						textAlign: "right",
 					}}
 				>
-					<div style={{ marginRight: "5px" }}>{mappedNumber && mappedNumber.toFixed(2) + " " + unit}</div>
+					<div style={{ marginRight: "5px" }}>{mappedNumber.toFixed(2) + " " + unit}</div>
 					<div
 						style={{
 							borderTop: "10px solid transparent",
 							borderBottom: "10px solid transparent",
-							borderLeft: `15px solid rgb(${mappedColour})`,
+							borderLeft: `20px solid rgb(${mappedColour})`,
 						}}
 					/>
 				</div>
@@ -75,7 +103,7 @@ const ColourChart = ({ test, mappedNumber, mappedColour }) => {
 				}}
 			>
 				{colourRange.map((mappedColour, index) => (
-					<div key={index} style={{ display: "flex", alignItems: "center" }}>
+					<div key={index} style={{ display: "flex" }}>
 						<div
 							style={{
 								marginLeft: "5px",
